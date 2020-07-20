ef0d2899f2a643e520a7068906b04979177932ea
Michael Corey <mikejcorey@gmail.com>
2020-07-20 16:47:40 -0500

    Moved calculation of most rolling averages to pandas at the end of timeseries process, and pandas now handles that CSV/JSON export.

--- stats/management/commands/dump_mn_statewide_timeseries.py
+++ stats/management/commands/dump_mn_statewide_timeseries.py
@@ -2,6 +2,7 @@ import os
 import csv
 import json
 import datetime
+import pandas as pd
 from datetime import timedelta
 
 from django.conf import settings
@@ -36,38 +37,26 @@ class Command(BaseCommand):
         # Deaths: Get max scrape date for each real date
         deaths_timeseries_values = {}
         deaths_reported_dates = StatewideDeathsDate.objects.all().values_list('reported_date', flat=True).distinct()
-        # death_records = StatewideDeathsDate.objects.filter(reported_date__in=deaths_reported_dates).order_by('scrape_date')
-        # TODO: How to calculate rolling averages
         for t in deaths_reported_dates:
             latest_record = StatewideDeathsDate.objects.filter(reported_date=t).values().latest('scrape_date')
             deaths_timeseries_values[t] = latest_record
-        # print(tests_timeseries_values)
-
-        # TODO: add rolling average
-        # items = StatewideTotalDate.objects.annotate(
-        #     case_chng_rolling=Window(
-        #         expression=Avg('cases_daily_change'),
-        #         order_by=F('scrape_date').asc(),
-        #         frame=RowRange(start=-7,end=0)
-        #     )
-        # )
 
         # Topline records: get all by date
-
         topline_timeseries_values = {}
-        topline_data = StatewideTotalDate.objects.annotate(
-            cases_daily_change_rolling=Window(
-                expression=Avg('cases_daily_change'),
-                order_by=F('scrape_date').asc(),
-                frame=RowRange(start=-6,end=0)
-            )
-        ).annotate(
-            hosp_total_daily_rolling=Window(
-                expression=Avg('hospitalized_total_daily_change'),
-                order_by=F('scrape_date').asc(),
-                frame=RowRange(start=-6,end=0)
-            )
-        ).values()
+        topline_data = StatewideTotalDate.objects.all().values()
+        # .annotate(
+        #     cases_daily_change_rolling=Window(
+        #         expression=Avg('cases_daily_change'),
+        #         order_by=F('scrape_date').asc(),
+        #         frame=RowRange(start=-6,end=0)
+        #     )
+        # ).annotate(
+        #     hosp_total_daily_rolling=Window(
+        #         expression=Avg('hospitalized_total_daily_change'),
+        #         order_by=F('scrape_date').asc(),
+        #         frame=RowRange(start=-6,end=0)
+        #     )
+        # ).values()
         for s in topline_data:
         # for s in StatewideTotalDate.objects.all().values():
             topline_timeseries_values[s['scrape_date']] = s
@@ -121,7 +110,7 @@ class Command(BaseCommand):
                 if current_date <= datetime.date(2020, 3, 28):  # Temp conditional for old test dates
                     new_tests = 0
                     total_tests = 0
-                    daily_pct_positive = ''
+                    daily_pct_positive = None
                 else:
                     if current_date - timedelta(days=1) in tests_timeseries_values:
                         tr = tests_timeseries_values[current_date - timedelta(days=1)]
@@ -143,7 +132,7 @@ class Command(BaseCommand):
                     if new_tests > 0 and topline_data['cases_daily_change'] is not None:  # Test for null on cases_daily_change e.g. July 4
                         daily_pct_positive = round((topline_data['cases_daily_change']*1.0) / new_tests, 3)
                     else:
-                        daily_pct_positive = ''
+                        daily_pct_positive = None
 
                 previous_total_tests = total_tests
 
@@ -156,7 +145,7 @@ class Command(BaseCommand):
                     'date': current_date.strftime('%Y-%m-%d'),
                     'total_confirmed_cases': total_cases,
                     'cases_daily_change': topline_data['cases_daily_change'],
-                    'cases_daily_change_rolling': round(topline_data['cases_daily_change_rolling'], 1),
+                    # 'cases_daily_change_rolling': round(topline_data['cases_daily_change_rolling'], 1),
                     'cases_newly_reported': topline_data['cases_newly_reported'],
                     'cases_removed': topline_data['removed_cases'],
                     'cases_sample_date': new_cases_sample_date,
@@ -168,10 +157,10 @@ class Command(BaseCommand):
                     'currently_hospitalized': topline_data['currently_hospitalized'],
                     'currently_in_icu': topline_data['currently_in_icu'],
                     'hosp_total_daily_change': topline_data['hospitalized_total_daily_change'],
-                    'hosp_total_daily_rolling': topline_data['hosp_total_daily_rolling'],
+                    # 'hosp_total_daily_rolling': topline_data['hosp_total_daily_rolling'],
                     'total_statewide_deaths': topline_data['cumulative_statewide_deaths'],
                     'new_statewide_deaths': new_deaths,
-                    'new_statewide_deaths_rolling': new_deaths_rolling,
+                    'new_statewide_deaths_rolling': new_deaths_rolling,  # Calculated during record creation
                     'total_statewide_recoveries': topline_data['cumulative_statewide_recoveries'],
                     'total_completed_tests': '' if current_date <= datetime.date(2020, 3, 28) else total_tests,
                     'new_completed_tests': '' if current_date <= datetime.date(2020, 3, 28) else new_tests,
@@ -183,12 +172,27 @@ class Command(BaseCommand):
 
             current_date += timedelta(days=1)
 
-        with open(os.path.join(settings.BASE_DIR, 'exports', 'mn_covid_data', 'mn_statewide_timeseries.csv'), 'w') as csvfile:
-            fieldnames = ['date', 'total_confirmed_cases', 'cases_daily_change', 'cases_daily_change_rolling', 'cases_newly_reported', 'cases_removed', 'cases_sample_date', 'cases_total_sample_date', 'total_hospitalized', 'currently_hospitalized', 'currently_in_icu', 'hosp_total_daily_change', 'hosp_total_daily_rolling', 'total_statewide_deaths', 'new_statewide_deaths', 'new_statewide_deaths_rolling', 'total_statewide_recoveries', 'total_completed_tests' , 'new_completed_tests', 'new_completed_tests_rolling', 'daily_pct_positive']
+        # Run through pandas to calculate rolling averages. For some of this it could be done in Django-land, but for positive pct, for example, it's hard to mix and match in a django query. So might as well do them all the same.
+        ts_df = pd.DataFrame(rows)
+        ts_df['daily_pct_positive_rolling'] = ts_df['daily_pct_positive'].rolling(window=7).mean().round(3)
+        ts_df['cases_daily_change_rolling'] = ts_df['cases_daily_change'].rolling(window=7).mean().round(1)
+        ts_df['hosp_total_daily_rolling'] = ts_df['hosp_total_daily_change'].rolling(window=7).mean().round(1)
 
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            writer.writerows(rows)
+        # Put in a nice order...
+        out_df = ts_df[[
+            'date', 'total_confirmed_cases', 'cases_daily_change', 'cases_daily_change_rolling', 'cases_newly_reported', 'cases_removed', 'cases_sample_date', 'cases_total_sample_date', 'total_hospitalized', 'currently_hospitalized', 'currently_in_icu', 'hosp_total_daily_change', 'hosp_total_daily_rolling', 'total_statewide_deaths', 'new_statewide_deaths', 'new_statewide_deaths_rolling', 'total_statewide_recoveries', 'total_completed_tests' , 'new_completed_tests', 'new_completed_tests_rolling', 'daily_pct_positive', 'daily_pct_positive_rolling'
+        ]]
+        # print(out_df)
+        out_df.to_csv(os.path.join(settings.BASE_DIR, 'exports', 'mn_covid_data', 'mn_statewide_timeseries.csv'), index=False)
+        out_df.to_json(os.path.join(settings.BASE_DIR, 'exports', 'mn_statewide_timeseries.json'), orient='records')
 
-        with open(os.path.join(settings.BASE_DIR, 'exports', 'mn_statewide_timeseries.json'), 'w') as jsonfile:
-            jsonfile.write(json.dumps(rows))
+
+        # with open(os.path.join(settings.BASE_DIR, 'exports', 'mn_covid_data', 'mn_statewide_timeseries.csv'), 'w') as csvfile:
+        #     fieldnames = ['date', 'total_confirmed_cases', 'cases_daily_change', 'cases_daily_change_rolling', 'cases_newly_reported', 'cases_removed', 'cases_sample_date', 'cases_total_sample_date', 'total_hospitalized', 'currently_hospitalized', 'currently_in_icu', 'hosp_total_daily_change', 'hosp_total_daily_rolling', 'total_statewide_deaths', 'new_statewide_deaths', 'new_statewide_deaths_rolling', 'total_statewide_recoveries', 'total_completed_tests' , 'new_completed_tests', 'new_completed_tests_rolling', 'daily_pct_positive']
+        #
+        #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        #     writer.writeheader()
+        #     writer.writerows(rows)
+        #
+        # with open(os.path.join(settings.BASE_DIR, 'exports', 'mn_statewide_timeseries.json'), 'w') as jsonfile:
+        #     jsonfile.write(json.dumps(rows))
